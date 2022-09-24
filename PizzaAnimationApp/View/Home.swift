608fe75c4f3d68d3cb99f04491d5c512de9b6d11
kosmos1308 <pasha13kosmos@gmail.com>
2022-09-24 22:29:11 +0300

    Added two buttons and title

--- PizzaAnimationApp/View/Home.swift
+++ PizzaAnimationApp/View/Home.swift
@@ -9,8 +9,54 @@ import SwiftUI
 
 struct Home: View {
     var body: some View {
-        Text("")
+        VStack {
+            HStack {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "square.grid.2x2")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .foregroundColor(.white)
+                        .frame(width: 20, height: 20)
+                }
+                    
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image("man")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 38, height: 38)
+                        .clipShape(Circle())
+                }
+            }
+            .overlay {
+                Text(attributedTitle)
+                    .font(.title)
+            }
+            
+            Text("Select Your Pizza")
+                .font(.caption)
+                .foregroundColor(.gray)
+                .padding(.top, 15)
+
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+        .padding(15)
     }
+    
+    // MARK: Attributed title
+    var attributedTitle: AttributedString {
+        var string = AttributedString("EATPIZZA")
+        if let range = string.range(of: "PIZZA") {
+            string[range].font = .system(size: 28, weight: .bold)
+        }
+        return string
+    }
+    
 }
 
 struct Home_Previews: PreviewProvider {
